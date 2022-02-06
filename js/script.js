8fa9c0af4d7c4bac3b518ad487c45788e5ed2304
tsionted <tsion580@gmail.com>
2022-02-05 21:05:32 -0800

    day2 chaleng

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 //single line comment
 /*
 */
+
 console.log('Tsion');
+
 console.log('my favorite food is bread');
+
+let letter ='supercalifragilisticexpialidocious';
+
+console.log(letter.length);
+console.log(5);
+let bignum =BigInt(12);
+console.log(bignum);
